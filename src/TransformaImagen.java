a47aad4ded4f15db01d9007c4e1ced80cc4be6f6
alu10467008 <alundu008@ieselcaminas.org>
2022-04-27 17:25:06 +0200

    FATALITY PERFECT KO EA SPORTS ITS IN DA GAME SEGAA

--- src/TransformaImagen.java
+++ src/TransformaImagen.java
@@ -52,26 +52,35 @@ public class TransformaImagen {
             value = bufferInput.read();
         }
     }
-        public void transformaBlancoNegro () throws IOException  {
-            // Transformar a una imagen en blanco y negro y guardar como *_bn.bmp
-            File blackwhitetheme = new File("Resources/Jada-Kingdom-1_o.bmp");
-            //Read
-            FileInputStream fileReader = new FileInputStream(f);
-            BufferedInputStream bufferInput = new BufferedInputStream(fileReader);
-            byte[] meta = bufferInput.readNBytes(54);
-            //Write
-            FileOutputStream fileWriter = new FileOutputStream(blackwhitetheme);
-            fileWriter.write(meta);
-            int change;
-            int value = bufferInput.read();
-            while (value != -1) {
-                change = value / 2;
-                fileWriter.write(change);
-                value = bufferInput.read();
-        }
 
-        private String getNombreSinExtension () {
-            //Devuelve el nombre del archivo f sin extensión
-            return "";
+    public void transformaBlancoNegro() throws IOException {
+        // Transformar a una imagen en blanco y negro y guardar como *_bn.bmp
+        File greyscale = new File("Resources/Jada-Kingdom-1_bn.bmp");
+        //Read
+        FileInputStream fileReader = new FileInputStream(f);
+        BufferedInputStream bufferInput = new BufferedInputStream(fileReader);
+        byte[] meta = bufferInput.readNBytes(54);
+        //Write
+        FileOutputStream fileWriter = new FileOutputStream(greyscale);
+        fileWriter.write(meta);
+        int change;
+        int value1 = bufferInput.read();
+        int value2 = bufferInput.read();
+        int value3 = bufferInput.read();
+        while (value1 != -1) {
+            change = (value1 + value2 + value3) / 3;
+            fileWriter.write(change);
+            fileWriter.write(change);
+            fileWriter.write(change);
+            value1 = bufferInput.read();
+            value1 = bufferInput.read();
+            value1 = bufferInput.read();
         }
     }
+
+    private String getNombreSinExtension() {
+        //Devuelve el nombre del archivo f sin extensión
+        String[] filename = this.f.getName().split("\\.");
+        return filename[0];
+    }
+}
